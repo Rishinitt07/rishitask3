8c4565c47c8f6b05a94ae5ff7943c9cd5e3f5bc3
Rishinitt07 <167533586+Rishinitt07@users.noreply.github.com>
2024-08-01 23:19:47 +0530

    Add files via upload

--- server/index.js
+++ server/index.js
@@ -1,3 +1,10 @@
+
+
+
+
+
+
+
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
@@ -5,8 +12,17 @@ const userModel = require("./models/Users");
 const bodyparser = require("body-parser");
 const lyricsfinder = require("lyrics-finder");
 const SpotifyWebApi = require("spotify-web-api-node");
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+const crypto = require('crypto');
+const secretKey1 = crypto.randomBytes(64).toString('hex');
+
+
 
 const app = express();
+const secretKey = secretKey1;
+console.log(secretKey);
+
 app.use(express.json());
 app.use(cors());
 app.use(bodyparser.json());
@@ -14,37 +30,121 @@ app.use(bodyparser.urlencoded({ extended: true }));
 
 mongoose.connect("mongodb://127.0.0.1:27017/users");
 
-app.post("/login", (req, res) => {
+// Register route
+// app.post('/register', async (req, res) => {
+//   const { username, password } = req.body;
+//   const hashedPassword = await bcrypt.hash(password, 10);
+//   const newUser = new userModel({ username, password: hashedPassword });
+//   await newUser.save();
+//   res.status(201).json({ message: 'User registered' });
+// });
+
+
+
+app.post('/info', async (req, res) => {
+  const { name,user, pass } = req.body;
+  try {
+    // Generate a salt
+    const salt = await bcrypt.genSalt(10);
+    // Hash the password with the salt
+    const hashedPassword = await bcrypt.hash(pass, salt);
+
+    // Save the user with the hashed password
+    const newUser = new userModel({ name,user, pass: hashedPassword });
+    await newUser.save();
+
+    res.status(201).json({ message: 'User registered' });
+  } catch (err) {
+    res.status(500).json({ message: 'Server error' });
+  }
+});
+
+
+
+// Login route
+app.post("/login", async (req, res) => {
   const { user, pass } = req.body;
-  userModel.findOne({ user: user }).then((user) => {
-    if (user) {
-      if (user.pass === pass) {
-        res.json("Success");
-        res.json(userinfo)
-        console.log(user.name);
-        
-        
+  const existingUser = await userModel.findOne({ user });
+  if (existingUser && await bcrypt.compare(pass, existingUser.pass)) {
+    const token = jwt.sign({ user }, secretKey, { expiresIn: '1h' });
+    console.log(token)
+    
 
-      } else {
-        res.json("The password is incorrect");
-      }
-    } 
     
-    
-    else {
-      res.json("No record found");
+    res.json({ token });
+    // res.send(token)
+   
+  
+    // res.status(201).json({ message: 'User registered' });
+  } else {
+    res.status(401).json({ message: 'Invalid credentials' });
+  }
+});
+
+// // Middleware to verify JWT token
+// const authenticateToken = (req, res, next) => {
+//   const token = req.headers['authorization'];
+//   if (!token) return res.status(401).json({ message: 'No token provided' });
+
+//   jwt.verify(token, secretKey, (err, decoded) => {
+//     if (err) return res.status(403).json({ message: 'Failed to authenticate token' });
+//     req.user = decoded.user;
+//     next();
+//   });
+// };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const authenticateToken = (req, res, next) => {
+  const authHeader = `${token}`
+  console.log(authHeader)
+  if (!authHeader) {
+    return res.status(401).json({ message: 'No token provided' });
+  }
+
+  const token = authHeader.split(' ')[1]; // Extract the token part
+  if (!token) {
+    return res.status(401).json({ message: 'Malformed token' });
+  }
+
+  jwt.verify(token, secretKey, (err, decoded) => {
+    if (err) {
+      console.error('Token verification error:', err);
+      return res.status(403).json({ message: 'Failed to authenticate token' });
     }
+    req.user = decoded.user;
+    next();
   });
+};
+
+
+app.get('/protected', authenticateToken, (req, res) => {
+  res.json({ message: `Welcome ${req.user}` });
 });
 
-app.post("/info", (req, res) => {
-  userModel
-    .create(req.body)
-    .then((userinfo) => res.json(userinfo))
-    .catch((err) => res.json(err));
-});
+// app.post("/info", (req, res) => {
+//   userModel
+//     .create(req.body)
+//     .then((userinfo) => res.json(userinfo))
+//     .catch((err) => res.json(err));
+// });
 
-app.post("/home", (req, res) => {
+app.post("/home", authenticateToken, (req, res) => {
   userModel
     .create(req.body)
     .then((home) => res.json(home))
@@ -96,13 +196,6 @@ app.post("/loginto", (req, res) => {
     });
 });
 
-
-
-
-
-
-
-
 app.post("/refreshplay", (req, res) => {
   const refreshToken = req.body.refreshToken;
   const spotifyApi = new SpotifyWebApi({
@@ -148,93 +241,56 @@ app.post("/logintoplay", (req, res) => {
     });
 });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 app.get("/lyrics", async (req, res) => {
-  const lyrics =
-    (await lyricsfinder(req.query.artist, req.query.track)) ||
-    "No Lyrics Found";
+  const lyrics = (await lyricsfinder(req.query.artist, req.query.track)) || "No Lyrics Found";
   res.json({ lyrics });
 });
 
-
-
-
-
-
-app.post("/friends", (req, res) => {
-  const {user} = req.body;
-  userModel.findOne({user : user }).then((user) => {
-    console.log(user)
+app.post("/friends", authenticateToken, (req, res) => {
+  const { user } = req.body;
+  userModel.findOne({ user }).then((user) => {
     if (user) {
-      
-        res.json("Success");
-        console.log(user);
-      
-      // else {
-      //   res.json("The password is incorrect");
-      // }
+      res.json("Success");
     } else {
       res.json("No record found");
-      console.log(user);
     }
   });
 });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 app.listen(3001, () => {
   console.log("server is running on port 3001");
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
