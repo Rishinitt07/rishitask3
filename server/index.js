46e212053cd0cd1a624502751c2259b3fee2dfc8
Rishinitt07 <167533586+Rishinitt07@users.noreply.github.com>
2024-08-03 23:24:23 +0530

    Add files via upload

--- server/index.js
+++ server/index.js
@@ -9,6 +9,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const userModel = require("./models/Users");
+const friendsModel= require("./models/friends")
 const bodyparser = require("body-parser");
 const lyricsfinder = require("lyrics-finder");
 const SpotifyWebApi = require("spotify-web-api-node");
@@ -16,30 +17,35 @@ const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
 const crypto = require('crypto');
 const cookieParser = require('cookie-parser');
+// const { default: Login } = require("../login/src/Login");
 const secretKey1 = crypto.randomBytes(64).toString('hex');
+const secretKey2 = crypto.randomBytes(64).toString('hex');
+// const Login = require('../login/src/Login.js');
+
+
 
 
 
 const app = express();
 app.use(cookieParser());
 const secretKey = secretKey1;
-console.log(secretKey);
+const secretKey3 = secretKey2
+// console.log(secretKey);
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin : ["http://localhost:5173"],
+  credentials:true
+  
+}
+
+));
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({ extended: true }));
 
 mongoose.connect("mongodb://127.0.0.1:27017/users");
 
-// Register route
-// app.post('/register', async (req, res) => {
-//   const { username, password } = req.body;
-//   const hashedPassword = await bcrypt.hash(password, 10);
-//   const newUser = new userModel({ username, password: hashedPassword });
-//   await newUser.save();
-//   res.status(201).json({ message: 'User registered' });
-// });
+
 
 
 
@@ -54,6 +60,8 @@ app.post('/info', async (req, res) => {
     // Save the user with the hashed password
     const newUser = new userModel({ name,user, pass: hashedPassword });
     await newUser.save();
+    const friends = new friendsModel({user});
+    await friends.save()
 
     res.status(201).json({ message: 'User registered' });
   } catch (err) {
@@ -68,13 +76,27 @@ app.post("/login", async (req, res) => {
   const { user, pass } = req.body;
   const existingUser = await userModel.findOne({ user });
   if (existingUser && await bcrypt.compare(pass, existingUser.pass)) {
-    const token = jwt.sign({ user }, secretKey, { expiresIn: '1h' });
+    const token = jwt.sign({ user }, secretKey, { expiresIn: '1m' });
+    const reftoken = jwt.sign({ user }, secretKey3, { expiresIn: '2m' });
+    res.cookie("accesstoken",token,{maxAge:60000})
+    res.cookie("refreshtoken",reftoken,{maxAge:120000,httpOnly:true,secure:true,sameSite:"strict"})
+
+    // const Login = require('../login/src/Login.js');
+
+
+
     console.log(token)
+    console.log(reftoken)
+    console.log(req.cookies.accesstoken)
+
+
+    // res.json({token})
+
+    return res.json({login:true})
+
     
 
     
-    res.json({ token });
-    // res.send(token)
    
   
     // res.status(201).json({ message: 'User registered' });
@@ -83,77 +105,59 @@ app.post("/login", async (req, res) => {
   }
 });
 
-// // Middleware to verify JWT token
-// const authenticateToken = (req, res, next) => {
-//   const token = req.headers['authorization'];
-//   if (!token) return res.status(401).json({ message: 'No token provided' });
-
-//   jwt.verify(token, secretKey, (err, decoded) => {
-//     if (err) return res.status(403).json({ message: 'Failed to authenticate token' });
-//     req.user = decoded.user;
-//     next();
-//   });
-// };
 
 
 
+const verifyuser = (req,res,next)=>{
+  const access_token = req.cookies.accesstoken
+  if(!access_token){
+    if(renewtoken(req,res)){
+      next()
 
+    }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const authenticateToken = (req, res, next) => {
-//   const authHeader = `${token}`
-//   console.log(authHeader)
-//   if (!authHeader) {
-//     return res.status(401).json({ message: 'No token provided' });
-//   }
-
-//   const token = authHeader.split(' ')[1]; // Extract the token part
-//   if (!token) {
-//     return res.status(401).json({ message: 'Malformed token' });
-//   }
-
-//   jwt.verify(token, secretKey, (err, decoded) => {
-//     if (err) {
-//       console.error('Token verification error:', err);
-//       return res.status(403).json({ message: 'Failed to authenticate token' });
-//     }
-//     req.user = decoded.user;
-//     next();
-//   });
-// };
-
-
-
-
-
-const authenticateJWT = (req, res, next) => {
-  const token = req.cookies.token;
-  console.log(token)
-
-  if (token) {
-    jwt.verify(token, SECRET_KEY, (err, user) => {
-      if (err) {
-        return res.sendStatus(403);
-      }
-      req.user = user;
-      next();
-    });
-  } else {
-    res.sendStatus(401);
   }
-};
+  else{
+    jwt.verify(access_token,secretKey,(err,decoded)=>{
+      if(err){
+        return res.json({valid:true,message:"Invalid token"})
+      }else{
+        req.user = decoded.user;
+        next()
+
+      }
+    })
+  }
+}
+const renewtoken = (req,res,next)=>{
+  const refresh_token = req.cookies.refreshtoken
+  let exist = false
+  if(!refresh_token){
+    return res.json({valid:false,message:"No Refresh token"})
+
+  }
+  else{
+    jwt.verify(refresh_token,secretKey3,(err,decoded)=>{
+      if(err){
+        return res.json({valid:true,message:"Invalid token"})
+      }else{
+        const token = jwt.sign({ user:decoded.user }, secretKey, { expiresIn: '1m' });
+        res.cookie("accesstoken",token,{maxAge:60000})
+        exist = true;
+
+
+      }
+    })
+  }
+  return exist;
+}
+
+
+
+
+app.get('/dashboard',verifyuser,(req,res)=>{
+  return res.json({valid:true,message:"authorised"})
+})
 
 
 
@@ -166,10 +170,50 @@ const authenticateJWT = (req, res, next) => {
 
 
 
-app.get('/protected', authenticateJWT, (req, res) => {
-  res.json({ message: `Welcome ${req.user}` });
 
-});
+
+
+
+
+
+
+
+
+
+
+
+// const authenticateJWT = (req, res, next) => {
+//   const token = req.cookies.token;
+//   console.log(token)
+
+//   if (token) {
+//     jwt.verify(token, SECRET_KEY, (err, user) => {
+//       if (err) {
+//         return res.sendStatus(403);
+//       }
+//       req.user = user;
+//       next();
+//     });
+//   } else {
+//     res.sendStatus(401);
+//   }
+// };
+
+
+
+
+
+
+
+
+
+
+
+
+// app.get('/protected', authenticateJWT, (req, res) => {
+//   res.json({ message: `Welcome ${req.user}` });
+
+// });
 
 // app.post("/info", (req, res) => {
 //   userModel
@@ -178,12 +222,12 @@ app.get('/protected', authenticateJWT, (req, res) => {
 //     .catch((err) => res.json(err));
 // });
 
-app.post("/home", authenticateJWT, (req, res) => {
-  userModel
-    .create(req.body)
-    .then((home) => res.json(home))
-    .catch((err) => res.json(err));
-});
+// app.post("/home", authenticateJWT, (req, res) => {
+//   userModel
+//     .create(req.body)
+//     .then((home) => res.json(home))
+//     .catch((err) => res.json(err));
+// });
 
 app.post("/refresh", (req, res) => {
   const refreshToken = req.body.refreshToken;
@@ -230,59 +274,59 @@ app.post("/loginto", (req, res) => {
     });
 });
 
-app.post("/refreshplay", (req, res) => {
-  const refreshToken = req.body.refreshToken;
-  const spotifyApi = new SpotifyWebApi({
-    redirectUri: "http://localhost:5173/playlist",
-    clientId: "b9e7f4a1fe48400790a362c6de406e4a",
-    clientSecret: "9b5b9d5c8edd41e7adb74f0ce3066b2e",
-    refreshToken,
-  });
+// app.post("/refreshplay", (req, res) => {
+//   const refreshToken = req.body.refreshToken;
+//   const spotifyApi = new SpotifyWebApi({
+//     redirectUri: "http://localhost:5173/playlist",
+//     clientId: "b9e7f4a1fe48400790a362c6de406e4a",
+//     clientSecret: "9b5b9d5c8edd41e7adb74f0ce3066b2e",
+//     refreshToken,
+//   });
 
-  spotifyApi
-    .refreshAccessToken()
-    .then((data) => {
-      res.json({
-        accessToken: data.body.accessToken,
-        expiresIn: data.body.expiresIn,
-      });
-    })
-    .catch((err) => {
-      console.log(err);
-      res.sendStatus(400);
-    });
-});
+//   spotifyApi
+//     .refreshAccessToken()
+//     .then((data) => {
+//       res.json({
+//         accessToken: data.body.accessToken,
+//         expiresIn: data.body.expiresIn,
+//       });
+//     })
+//     .catch((err) => {
+//       console.log(err);
+//       res.sendStatus(400);
+//     });
+// });
 
-app.post("/logintoplay", (req, res) => {
-  const code = req.body.code;
-  const spotifyApi = new SpotifyWebApi({
-    redirectUri: "http://localhost:5173/playlist",
-    clientId: "b9e7f4a1fe48400790a362c6de406e4a",
-    clientSecret: "9b5b9d5c8edd41e7adb74f0ce3066b2e",
-  });
+// app.post("/logintoplay", (req, res) => {
+//   const code = req.body.code;
+//   const spotifyApi = new SpotifyWebApi({
+//     redirectUri: "http://localhost:5173/playlist",
+//     clientId: "b9e7f4a1fe48400790a362c6de406e4a",
+//     clientSecret: "9b5b9d5c8edd41e7adb74f0ce3066b2e",
+//   });
 
-  spotifyApi
-    .authorizationCodeGrant(code)
-    .then((data) => {
-      res.json({
-        accessToken: data.body.access_token,
-        refreshToken: data.body.refresh_token,
-        expiresIn: data.body.expires_in,
-      });
-    })
-    .catch((err) => {
-      res.sendStatus(400);
-    });
-});
+//   spotifyApi
+//     .authorizationCodeGrant(code)
+//     .then((data) => {
+//       res.json({
+//         accessToken: data.body.access_token,
+//         refreshToken: data.body.refresh_token,
+//         expiresIn: data.body.expires_in,
+//       });
+//     })
+//     .catch((err) => {
+//       res.sendStatus(400);
+//     });
+// });
 
 app.get("/lyrics", async (req, res) => {
   const lyrics = (await lyricsfinder(req.query.artist, req.query.track)) || "No Lyrics Found";
   res.json({ lyrics });
 });
 
-app.post("/friends", authenticateJWT, (req, res) => {
+app.post("/friends",  (req, res) => {
   const { user } = req.body;
-  userModel.findOne({ user }).then((user) => {
+  friendsModel.findOne({ user }).then((user) => {
     if (user) {
       res.json("Success");
     } else {
